86cefd6d5f4a90219223fbfe62a7d8bd99fd36c7
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-01 19:59:01 +0600

    variable in java Script

---
+++ 5variable-declaration.js
@@ -0,0 +1,13 @@
+
+// var firstName = "Anisul ";
+// var lastName = "Islam";
+// var age = 25 ;
+
+var firstName,lastName,age ;
+firstName="anisul";
+lastName = "islam";
+age=24;
+
+
+
+document.write(firstName + "</br>" + lastName +"</br>"+age);
